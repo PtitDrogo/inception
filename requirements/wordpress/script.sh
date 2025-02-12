97f5bf5ed3b0d0fb8b93477a6f99cceea2fa6551
Theo Freydiere <tfreydie@debian.tfreydie>
2025-02-12 12:58:39 -0600

    problems with db

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -4,12 +4,24 @@ touch tfreydiewashere.txt
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar config create \
+    --dbname=wordpress \
+    --dbuser=wpuser \
+    --dbpass=password \
+    --dbhost=mariadb \
+    --allow-root \
+--extra-php <<PHP
+define('WP_HOME', 'https://tfreydie.42.fr');
+define('WP_SITEURL', 'https://tfreydie.42.fr');
+PHP
+
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
 #On a l'air de creer la DB, l'admin de wp, mais pas de users.
-# a demander a chat gpt pour la continuite
 
 #on installe pas de theme
+
+#on installe un theme pour voir se qu'il se passe 
+./wp-cli.phar theme install astra --activate --allow-root
 
 php-fpm7.4 -F
