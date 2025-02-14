d51e6d88b3fdabae61ff5b230591a7cf4199389b
llitot <lenylitot@gmail.com>
2025-02-14 05:45:04 -0600

    .env coms

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -4,24 +4,12 @@ touch tfreydiewashere.txt
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create \
-    --dbname=wordpress \
-    --dbuser=wpuser \
-    --dbpass=password \
-    --dbhost=mariadb \
-    --allow-root \
---extra-php <<PHP
-define('WP_HOME', 'https://tfreydie.42.fr');
-define('WP_SITEURL', 'https://tfreydie.42.fr');
-PHP
-
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}" --allow-root
+./wp-cli.phar core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
 
 #On a l'air de creer la DB, l'admin de wp, mais pas de users.
+# a demander a chat gpt pour la continuite
 
 #on installe pas de theme
-
-#on installe un theme pour voir se qu'il se passe 
-./wp-cli.phar theme install astra --activate --allow-root
 
 php-fpm7.4 -F
