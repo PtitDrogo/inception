218f9a1287c82c8edc10958ed8f4b8e70e98f960
Theo Freydiere <tfreydie@debian.tfreydie>
2025-02-14 06:26:59 -0600

    added back php that disappeared for some reason

--- requirements/wordpress/script.sh
+++ requirements/wordpress/script.sh
@@ -4,7 +4,12 @@ touch tfreydiewashere.txt
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}" --allow-root
+./wp-cli.phar config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASSWORD}" --dbhost="${DB_HOST}" --allow-root \
+--extra-php <<PHP
+define('WP_HOME', 'https://tfreydie.42.fr');
+define('WP_SITEURL', 'https://tfreydie.42.fr');
+PHP
+
 ./wp-cli.phar core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
 
 #On a l'air de creer la DB, l'admin de wp, mais pas de users.
