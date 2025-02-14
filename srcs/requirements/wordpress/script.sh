c4cdd18cbab4e3751f9c404ad57be04467e594fa
Theo Freydiere <tfreydie@debian.tfreydie>
2025-02-14 11:11:03 -0600

    changed some passwords

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /var/www/html
-touch tfreydiewashere.txt
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
@@ -11,10 +10,5 @@ define('WP_SITEURL', 'https://tfreydie.42.fr');
 PHP
 
 ./wp-cli.phar core install --url="${WP_URL}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --allow-root
-
-#On a l'air de creer la DB, l'admin de wp, mais pas de users.
-# a demander a chat gpt pour la continuite
-
-#on installe pas de theme
 
 php-fpm7.4 -F
